28ca190b749dcf62876347b1eefc833be683ec9f
wangping <wang.ping6@zte.com.cn>
2019-04-03 10:06:33 +0800

    add spin

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,9 @@
 import { NgModule} from '@angular/core';
+import { CommonModule} from '@angular/common';
 import { SpinComponent} from './spin/spin.component';
 
 @NgModule({
+    imports: [CommonModule],
     declarations : [SpinComponent],
        exports  : [SpinComponent]
 })
