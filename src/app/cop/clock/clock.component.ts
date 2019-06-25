9d22f18e21ab20866ec86309072127ce3d98fa9b
wangping <wang.ping6@zte.com.cn>
2019-06-25 16:51:41 +0800

    add clock

--- src/app/cop/clock/clock.component.ts
+++ src/app/cop/clock/clock.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clock.component.less']
 })
 export class ClockComponent implements OnInit {
-
+  second: number;
+  minute: number;
+  hour: number;
   constructor() { }
 
   ngOnInit() {
+    this.initTime();
+    setInterval(() => {
+      this.initTime();
+    }, 1000);
   }
 
+  initTime() {
+    const time = new Date();
+    this.second = time.getSeconds();
+    this.minute = time.getMinutes() + this.second / 60;
+    this.hour =  time.getHours() + this.minute / 60;
+  }
+
+  setHour() {
+    return {
+      'transform': `rotate(${this.hour * 30 - 90}deg)`
+    };
+  }
+
+  setMinute() {
+    return {
+      'transform': `rotate(${this.minute * 6 - 90}deg)`
+    };
+  }
+
+  setSecond() {
+    return {
+      'transform': `rotate(${this.second * 6 - 90}deg)`
+    };
+  }
 }
